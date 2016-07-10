890ef25b8ed0b28d006872b4e3d8cfd75eed41af
Art Zemon <art@zemon.name>
2016-07-10 14:47:56 -0500

    Remove local Hebrew text of Torah, used for searching for words in the Torah. Removed Bible model, data, etc.

--- bible/data/gettext.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for book in Genesis Exodus Leviticus Numbers Deuteronomy
-do
-    wget -nv -O $book-hebrew.json "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Tanakh/Torah/$book/Hebrew/Tanach%20with%20Nikkud.json"
-done
