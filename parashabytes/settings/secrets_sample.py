19f8fed2bca566a2b1e8cde2e3a1feadf4473c27
Art Zemon <art@zemon.name>
2016-07-13 08:09:14 -0500

    add sample secrets.py settings file

---
+++ parashabytes/settings/secrets_sample.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '50 character long secret key'
+
+# local development copy
+db = {
+    'hostname': 'hostname',
+    'username': 'username',
+    'password': 'password',
+    'database': 'database_name',
+}
